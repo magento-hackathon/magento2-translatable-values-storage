ad38380fe3c594a99d77c129df73fda21e61a372
Justin van Elst <justinvanelst@gmail.com>
2016-09-24 14:38:42 +0200

    Repository

---
+++ Model/ValuesRepositoryModel.php
@@ -0,0 +1,38 @@
+<?php
+namespace Hackathon\Translationstorage\Model;
+
+class ValuesRepositoryModel implements \Hackathon\Translationstorage\Api\ValuesRepositoryInterface
+{
+
+    /**
+     * Save test data.
+     */
+    public function save(\Hackathon\Translationstorage\Api\Data\ValuesInterface $test)
+    {
+//your code
+    }
+
+    /**
+     * Retrieve test data.
+     */
+    public function getById($testId)
+    {
+//your code
+    }
+
+    /**
+     * Delete test.
+     */
+    public function delete(\Hackathon\Translationstorage\Api\Data\ValuesInterface $test)
+    {
+//your code
+    }
+
+    /**
+     * Delete test by test ID.
+     */
+    public function deleteById($testId)
+    {
+//your code
+    }
+}
